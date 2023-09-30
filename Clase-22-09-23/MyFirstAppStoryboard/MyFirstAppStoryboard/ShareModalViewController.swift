54e450c1cb6a0f82088dd17d077f20660c23ce06
Uriel Soubran <uriel.soubran@gmail.com>
2023-09-30 01:23:52 -0600

    Add new behaviors to TextView
    Things added to Share TextView:
    - WordCount
    - maxWordCount
    - Border

--- Clase-22-09-23/MyFirstAppStoryboard/MyFirstAppStoryboard/ShareModalViewController.swift
+++ Clase-22-09-23/MyFirstAppStoryboard/MyFirstAppStoryboard/ShareModalViewController.swift
@@ -9,16 +9,45 @@ import UIKit
 
 class ShareModalViewController: UIViewController {
     
-    @IBOutlet weak var textViewField: UITextView!
-
+    @IBOutlet weak var textViewField: UITextView! {
+        didSet {
+            textViewField.delegate = self
+        }
+    }
+    @IBOutlet weak var wordCountLabel: UILabel!
+    
+    let maxWordCount = 150
+    
+    //Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         // Do any additional setup after loading the view.
+        
+        // create a border for a view (TextView in this case)
+        let borderColor : UIColor = UIColor(red: 0.85, green: 0.85, blue: 0.85, alpha: 1.0)
+        textViewField.layer.borderWidth = 0.5
+        textViewField.layer.borderColor = borderColor.cgColor
+        textViewField.layer.cornerRadius = 5.0
+        
+        //set wordCountLabel first state
+        wordCountLabel.text = "0 / 150"
     }
 
+//    Aplicar una acción cuando el botón de share se oprime.
     @IBAction func onSendButtonClicked(_ sender: UIButton) {
         guard let textBox = textViewField.text else {return}
         print(textBox)
+    }
+}
+
+//delegates
+extension ShareModalViewController: UITextViewDelegate {
+    func textViewDidChange(_ textView: UITextView) {
+        wordCountLabel.text = "\(textView.text.count) / \(maxWordCount)"
+    }
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        if text == "" {return true}
+        return (textView.text?.count ?? 0) + text.count - range.length <= maxWordCount
     }
 }
