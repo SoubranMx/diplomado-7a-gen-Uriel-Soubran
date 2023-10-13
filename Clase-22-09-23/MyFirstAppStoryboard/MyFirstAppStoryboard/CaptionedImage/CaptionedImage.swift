9d531868dbdf6f6243866cc473fb94b1e82cef49
Uriel Soubran <uriel.soubran@gmail.com>
2023-10-13 17:19:43 -0600

    XID

---
+++ Clase-22-09-23/MyFirstAppStoryboard/MyFirstAppStoryboard/CaptionedImage/CaptionedImage.swift
@@ -0,0 +1,20 @@
+//
+//  CaptionedImage.swift
+//  MyFirstAppStoryboard
+//
+//  Created by Diplomado on 13/10/23.
+//
+
+import UIKit
+
+class CaptionedImage: UIView {
+    //esto define que es lo que tiene el XID
+    @IBOutlet weak var captionLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    //con xids, necesitamos inicializar los elementos o crashea cuando intentas acceder a los outlets
+    
+    
+    
+
+}
--- Clase-22-09-23/MyFirstAppStoryboard/MyFirstAppStoryboard/ImageStackViewController.swift
+++ Clase-22-09-23/MyFirstAppStoryboard/MyFirstAppStoryboard/ImageStackViewController.swift
@@ -12,22 +12,22 @@ class ImageStackViewController: UIViewController {
     var showDogs = false
 // IBOutlet es una manera de relacionar elementos del Storyboard con variables de código. Se puede arrastrar el inicio de la variable hacia el elemento que quieres relacionar.
     
-    @IBOutlet weak var thirdImageView: UIImageView!
-    @IBOutlet weak var secondImageView: UIImageView!
-    @IBOutlet weak var firstImageView: UIImageView!
+    @IBOutlet weak var thirdCaptionedImage: CaptionedImage!
+    @IBOutlet weak var secondCaptionedImage: CaptionedImage!
+    @IBOutlet weak var firstCaptionedImage: CaptionedImage!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 //      UIImage es un contenedor de imagenes
 //      Crea una imagen teniendo lo que se encuentra dentro de assets con el nombre dog-1
         if showDogs {
-            firstImageView.image = UIImage(named: "dog-1")
-            secondImageView.image = UIImage(named: "dog-2")
-            thirdImageView.image = UIImage(named: "dog-3")
+            firstCaptionedImage.imageView.image = UIImage(named: "dog-1")
+            secondCaptionedImage.imageView.image = UIImage(named: "dog-2")
+            thirdCaptionedImage.imageView.image = UIImage(named: "dog-3")
         } else {
-            firstImageView.image = UIImage(named: "cat-1")
-            secondImageView.image = UIImage(named: "cat-2")
-            thirdImageView.image = UIImage(named: "cat-3")
+            firstCaptionedImage.imageView.image = UIImage(named: "cat-1")
+            secondCaptionedImage.imageView.image = UIImage(named: "cat-2")
+            thirdCaptionedImage.imageView.image = UIImage(named: "cat-3")
         }
     }
 }
