99c0f6067f8ce24bdd46c097e390a9c6c3963689
Uriel Soubran <uriel_bee15@hotmail.com>
2023-10-17 22:22:24 -0600

    ME2 - Main Config XIB behavior complete
    For record: ME2 stands for Module Exercise number,
    in this case, Module Exercise 2 (for second final module exercise)
    
    Added:
    - Fix uiview conflicts.
    When stacks were child of a uiview, the objects inside were broken and couldn't be interacted with.
    Made the stacks behave as the uiview in terms of constraints. (may be redundant to use a uiview to wrap the elements inside instead of just using the stack)
    
    - byPoints TextField Delegates to accept numeric and backspace
    
    - byRounds slider change by steps and show the string number
    
    - Make selectedMode an enum to avoid miss spelling when writing. Two cases: .points and .rounds
    
    - PlayButton visibility depending on selectedMode and if it is .points, PlayButton hides if at least one TextField is empty, shows if all TextFields (including Player's Name) have a value.
    If it's .rounds, check only for Player's Name TextField.
    
    To add:
    - The next views: Info, Game and History
    - Prepare segue to InfoView and GameView from Configuration

--- Final Module Exercises/SecondFinal_RockPaperScissors/SecondFinal_RockPaperScissors/src/Configuration/ConfigurationViewController.swift
+++ Final Module Exercises/SecondFinal_RockPaperScissors/SecondFinal_RockPaperScissors/src/Configuration/ConfigurationViewController.swift
@@ -10,21 +10,50 @@ import UIKit
 class ConfigurationViewController: UIViewController {
 
     @IBOutlet weak var GameTypeControl: UISegmentedControl!
-    @IBOutlet weak var RoundsView: UIView!
-    @IBOutlet weak var PointsView: UIView!
+    @IBOutlet weak var RoundsStackView: UIStackView!
+    @IBOutlet weak var PointsStackView: UIStackView!
     @IBOutlet weak var PlayerNameTextField: UITextField!
     @IBOutlet weak var PlayButton: UIButton!
-    @IBOutlet weak var PointsPerWinTextField: UITextField!
-    @IBOutlet weak var PointsPerLoseTextField: UITextField!
-    @IBOutlet weak var PointsToWinTextField: UITextField!
+    //since these textFields are custom controlled, they need to be delegated
+    @IBOutlet weak var PointsPerWinTextField: UITextField! {
+        didSet {
+            PointsPerWinTextField.delegate = self
+        }
+    }
+    @IBOutlet weak var PointsPerLoseTextField: UITextField! {
+        didSet {
+            PointsPerLoseTextField.delegate = self
+        }
+    }
+    @IBOutlet weak var PointsToWinTextField: UITextField! {
+        didSet {
+            PointsToWinTextField.delegate = self
+        }
+    }
     @IBOutlet weak var RoundsSlider: UISlider!
     @IBOutlet weak var RoundsCountLabel: UILabel!
     
+    //To not have errors when writting the value of selectedMode, such as "Points" over "points", or miss spelled text, make it an enum
+    enum mode: String{
+        case points
+        case rounds
+    }
+    var selectedMode: mode = .points;
+    
+    
+    
     func initSetup(){
-        RoundsView.isHidden = true
+        RoundsStackView.isHidden = true
+        PlayButton.tintColor = UIColor.systemOrange
         PlayButton.isHidden = true
         RoundsCountLabel.text = "\(Int(RoundsSlider.value))"
+        PlayerNameTextField.text = ""
+        PointsPerWinTextField.text = ""
+        PointsPerLoseTextField.text = ""
+        PointsToWinTextField.text = ""
+        selectedMode = .points
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         initSetup()
@@ -33,17 +62,41 @@ class ConfigurationViewController: UIViewController {
     
     @IBAction func controlChanged(_ sender: UISegmentedControl) {
         if(sender.selectedSegmentIndex == 0){
-            RoundsView.isHidden = true
-            PointsView.isHidden = false
+            RoundsStackView.isHidden = true
+            PointsStackView.isHidden = false
+            selectedMode = .points
         } else {
-            RoundsView.isHidden = false
-            PointsView.isHidden = true
+            RoundsStackView.isHidden = false
+            PointsStackView.isHidden = true
+            selectedMode = .rounds
         }
+        decidePlayButtonVisibility()
     }
     
     @IBAction func onSliderChange(_ sender: UISlider) {
-        RoundsCountLabel.text = "\(Int(sender.value))"
-        RoundsSlider.value = sender.value
+        let roundedValue = round(sender.value)
+        sender.value = roundedValue
+        RoundsCountLabel.text = String(Int(roundedValue))
+    }
+    
+    func decidePlayButtonVisibility() {
+        if(selectedMode == .points) {
+            if((PointsPerWinTextField.text?.count ?? 0) > 0 && (PointsPerLoseTextField.text?.count ?? 0) > 0 && (PointsToWinTextField.text?.count ?? 0) > 0 && (PlayerNameTextField.text?.count ?? 0) > 0) {
+                PlayButton.isHidden = false
+            } else {
+                PlayButton.isHidden = true
+            }
+        } else {
+            if ((PlayerNameTextField.text?.count ?? 0) > 0) {
+                PlayButton.isHidden = false
+            } else {
+                PlayButton.isHidden = true
+            }
+        }
+    }
+    
+    @IBAction func onPlayerNameTextChange(_ sender: UITextField) {
+        decidePlayButtonVisibility()
     }
     
     
@@ -57,4 +110,37 @@ class ConfigurationViewController: UIViewController {
     }
     */
 
+}
+
+extension ConfigurationViewController: UITextFieldDelegate {
+//    to know when the textField is being edited
+    func textFieldDidChangeSelection(_ textField: UITextField) {
+        switch textField.tag {
+        case 0:
+            PointsPerWinTextField.text = textField.text
+            break
+        case 1:
+            PointsPerLoseTextField.text = textField.text
+            break
+        case 2:
+            PointsToWinTextField.text = textField.text
+            break
+        default:
+            return
+        }
+        
+        decidePlayButtonVisibility()
+    }
+    
+//    to control what kind of text the textField accepts
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+//        allow backspace to edit
+        if string == "" {return true}
+//        only allow digits
+        if let x = string.rangeOfCharacter(from: NSCharacterSet.decimalDigits) {
+            return true
+        } else {
+            return false
+        }
+    }
 }
