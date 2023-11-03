96d73d170b3ef7cd408630f2285ff69ad170b44b
Uriel Soubran <uriel.soubran@gmail.com>
2023-11-03 17:20:42 -0600

    CM3 - TabBarController
    Embed viewController in TabBarViewController, to add more, add segue like "view controllers"
    
    Image is the image shown when not selected, selected Image is the one that shows when you are on the tab.

--- Clase-03-11-23/SearchBar/SearchBar/ViewController.swift
+++ Clase-03-11-23/SearchBar/SearchBar/ViewController.swift
@@ -8,12 +8,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var filterArray = [String]()
+    var dataArray = ["Primero", "Segundo", "Tercero"]
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        filterArray = dataArray
+        tableView.dataSource = self
+        searchBar.delegate = self
     }
 
+    @IBAction func sortButton(_ sender: UIBarButtonItem) {
+        if tableView.isEditing {
+            tableView.isEditing = false
+        } else {
+            tableView.isEditing = true
+        }
+    }
+}
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)->Int{
+        filterArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        if cell == nil {
+            cell = UITableViewCell(style: .subtitle, reuseIdentifier: "cell")
+        }
+        cell?.textLabel?.text = filterArray[indexPath.row]
+        return cell!
+    }
+    
+//    Para permitir mover las rows
+    func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        true
+    }
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+//        swap intercambia internamente las rows y hace un reloadData
+        filterArray.swapAt(sourceIndexPath.row, destinationIndexPath.row)
+    }
+}
+
+extension ViewController: UISearchBarDelegate {
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+        filterArray = []
+        
+        if searchText == "" {
+            filterArray = dataArray
+        }
+        
+        for word in dataArray {
+            if word.uppercased().contains(searchText.uppercased()) {
+                filterArray.append(word)
+            }
+        }
+        
+        tableView.reloadData()
+    }
 }
 
