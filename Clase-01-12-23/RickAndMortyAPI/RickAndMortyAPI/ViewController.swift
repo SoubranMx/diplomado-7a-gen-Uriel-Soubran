db11e4e687462f17b4865f059ec1a33d2c61b478
Uriel Soubran <uriel.soubran@gmail.com>
2023-12-01 19:31:14 -0600

    CM5 - Rick And Morty API init

---
+++ Clase-01-12-23/RickAndMortyAPI/RickAndMortyAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyAPI
+//
+//  Created by Diplomado on 01/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
